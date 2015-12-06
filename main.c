dea2b85f7aa3958955e057fc4dfc246d3a4cd0ab
Levhita <levhita@gmail.com>
2015-12-05 19:40:14 -0600

    Updated license in main.c

--- main.c
+++ main.c
@@ -1,10 +1,21 @@
 /*
-* Copyright 2002-2010 Guillaume Cottenceau.
-*
-* This software may be freely redistributed under the terms
-* of the X11 license.
-*
-*/
+ * TimeShifter: given a set of standalone frames, creates a sequence of them shifted in time.
+ * Copyright (C) 2015 Argel Arias <levhita@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+ */
 
 #include <unistd.h>
 #include <stdlib.h>
