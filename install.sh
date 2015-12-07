1548984e0ee2fc242f3e05784ab12b4dfd7a5aa3
Levhita <levhita@gmail.com>
2015-12-06 21:03:00 -0600

    added install.sh file

---
+++ install.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+usage="Installs timeshifter to ~/.bin directory
+
+Usage: install.sh [-options]
+
+Where options can be
+-c	Compile timeshifter_core
+-u	Copy the executables to ~/.bin
+-e 	Adds the export line at the end of the .bashrc"
+
+hflag='yes'
+cflag='no'
+uflag='no'
+eflag='no'
+#Go to your home directory and append the export line to .bashrc.
+while getopts "cue" opt; do
+	case $opt in
+		c)
+			hflag='no'
+			cflag='yes'
+			;;
+		u)	
+			hflag='no'
+			uflag='yes'
+			;;
+
+		e)	
+			hflag='no'
+			eflag='yes'
+			;;
+	esac
+done
+
+shift $(expr $OPTIND - 1 )
+
+if [ $hflag = yes ]; then
+	echo "$usage">&2
+	exit 1
+fi
+if [ $cflag = yes ]; then
+	gcc main.c -lpng -o timeshifter_core
+fi
+
+if [ $uflag = yes ]; then
+	if [ ! -f timeshifter_core  ]; then
+		echo "timeshifter_core hasn't been compiled, please run the installer again with the -c flag">&2
+		exit 1
+	fi	
+	#Make a .bin dir in your home and copy timeshifter on it.
+	if [ ! -d ~/.bin  ]; then
+		mkdir ~/.bin 
+	fi
+	cp timeshifter ~/.bin/
+	cp timeshifter_core ~/.bin/
+fi
+
+if [ $eflag = yes ]; then
+	cd ~
+	echo 'export PATH=$PATH:$HOME/.bin/' >> .bashrc
+fi
+
+
